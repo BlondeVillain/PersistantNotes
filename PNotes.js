084774247053b1273b59c8231ad25999d6ec8e11
BlondeVillain <bradley.shadowens@gmail.com>
2018-08-07 11:57:36 -0500

    First Commit

---
+++ PNotes.js
@@ -0,0 +1,4 @@
+//Lecture Notes
+//https://www.digitalocean.com/community/tutorials/how-to-write-conditional-statements-in-javascript
+//Learn your ARROW FUNCTIONS, IF WITH ?, ELSE WITH :
+//Play with .slice .splice .push .pull .filter .map
